9fbb5b3fc40dc6b1f342c1ea0b808343b1033a4f
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2025-06-04 09:32:08 +0200

    Add automated reference detection code

--- src/cram_archiver/__init__.py
+++ src/cram_archiver/__init__.py
@@ -1,10 +1,16 @@
 import subprocess
 
+# 3.1 Not supported by some tools currently (2025)
+DEFAULT_CRAM_VERSION = "3.0"
+
+
+
 def convert_to_cram(
         input_file: str,
         output_file: str,
+        reference: str,
         threads: int = 1,
-        cram_version: str = "3.0",
+        cram_version: str = DEFAULT_CRAM_VERSION,
         write_index: bool = True,
 ):
     additional_threads = max(0, threads - 1)
@@ -12,7 +18,10 @@ def convert_to_cram(
         "samtools", "view",
         "--output-fmt", f"cram,version={cram_version}",
         "--threads", str(additional_threads),
-
+        "-o", output_file,
+        "--reference", reference,
+        input_file,
     ]
-    subprocess.run(
-    )
+    if write_index:
+        command.append("--write-index")
+    subprocess.run(command, check=True)
---
+++ src/cram_archiver/references.py
@@ -0,0 +1,97 @@
+"""
+This module contains utilities to handle different references within one
+application.
+"""
+import gzip
+import io
+import struct
+from typing import TextIO
+
+
+class ReferenceID:
+    """
+    A wrapper class for a string which contains contigs in tabular format
+    with the first column the contig name and the second the contig length.
+
+    The order of the contigs and their lengths makes for a unique string which
+    can be used as a unique ID for different versions of genome builds.
+    """
+    _id: str
+
+    def __init__(self, reference_id):
+        self._id = reference_id
+
+    def __eq__(self, other):
+        if not isinstance(other, ReferenceID):
+            raise TypeError(
+                f"Can only compare with instances of ReferenceID, got "
+                f"{other.__class__.__name__}")
+        return self._id == other._id
+
+    def __hash__(self):
+        return hash(self._id)
+
+    @property
+    def id(self) -> str:
+        return self._id
+
+    @classmethod
+    def from_file(cls, file: str):
+        """
+        Read the reference ID from the file. Auto-detects format based on the
+        magic bytes.
+        """
+        with open(file, "rb") as filehandle:  # type: io.BufferedReader
+            if filehandle.peek(2)[:2] == b"\x1f\x8b":
+                # Gzip magic detected.
+                filehandle = gzip.open(filehandle, "rb")  # type: ignore
+            magic = filehandle.peek(100)
+            text_handle = io.TextIOWrapper(filehandle)
+            if magic.startswith(b"@HD"):
+                return cls._from_sam_header(text_handle)
+            elif magic.startswith(b"BAM\x01"):
+                # Skip magic 4 bytes. The following 4 bytes are the length of
+                # the header in plaintext.
+                l_text, = struct.unpack("<xxxxI", filehandle.read(8))
+                # Only ASCII is allowed in the SAM/BAM header.
+                text = filehandle.read(l_text).decode("ascii")
+                return cls._from_sam_header(io.StringIO(text))
+            # FAI detection
+            first_line = magic.splitlines()[0]
+            if first_line.count(b"\t") == 4:
+                first_line_text = first_line.decode("ascii")
+                contig, contig_length, start, nucs_per_line, line_width = \
+                    first_line_text.split("\t")
+                if (contig_length.isdecimal() and
+                        start.isdecimal() and
+                        nucs_per_line.isdecimal() and
+                        line_width.isdecimal()):
+                    # This is a fasta index.
+                    return cls._from_fasta_index(text_handle)
+            raise NotImplementedError(f"file with magic {magic[:10]!r} not "
+                                      f"implemented.")
+    @classmethod
+    def _from_sam_header(cls, filehandle: TextIO):
+        """Parses the contigs and lengths from SAM header @SQ lines."""
+        id_build = io.StringIO()
+        for line in filehandle:
+            if not line.startswith("@"):
+                break
+            if line.startswith("@SQ"):
+                line_parts = line.strip().split("\t")
+                # line_parts from index 1 to remove @SQ part.
+                info_parts = (part.split(":", maxsplit=1) for part in line_parts[1:])
+                info_dict = dict(info_parts)
+                contig = info_dict["SN"]
+                contig_length = info_dict["LN"]
+                id_build.write(f"{contig}\t{contig_length}\n")
+        return cls(id_build.getvalue())
+
+    @classmethod
+    def _from_fasta_index(cls, filehandle: TextIO):
+        """Parses the contigs and lengths from the fasta index tabular format."""
+        id_build = io.StringIO()
+        for line in filehandle:
+            contig, contig_length, *rest = line.split()
+            id_build.write(f"{contig}\t{contig_length}\n")
+        return cls(id_build.getvalue())
